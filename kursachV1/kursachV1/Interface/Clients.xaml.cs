8f6302e22134032661e786c6b99081699130fb34
MarauderMavericK <Maverick_93@mail.ru>
2015-11-14 13:17:19 +0300

    f

---
+++ kursachV1/kursachV1/Interface/Clients.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace kursachV1.Interface
+{
+    /// <summary>
+    /// Interaction logic for Clients.xaml
+    /// </summary>
+    public partial class Clients : Window
+    {
+        public Clients()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clic_Menu(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+        private void Button_Click_24(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_562(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_YD(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_OB(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void DatePicker_DateValidationError(object sender, DatePickerDateValidationErrorEventArgs e)
+        {
+            /*VseKlient k = new VseKlient();
+            lblError.Text = k.DateValidationError(sender, e);*/
+        }
+    }
+}
---
+++ kursachV1/kursachV1/Interface/Employees.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace kursachV1.Interface
+{
+    /// <summary>
+    /// Interaction logic for Employees.xaml
+    /// </summary>
+    public partial class Employees : Window
+    {
+        public Employees()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clic_Menu(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_ClickOb(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
